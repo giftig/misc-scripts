06226f2e4de5ef1b7d3c371107e3506aaa31c7a1
Rob Moore <robert.moore@bglgroup.co.uk>
2018-09-13 13:57:39 +0100

    Update kafka scripts
    Small docs fix + add the ability for the producer to require replication

--- kafka/producer.sh
+++ kafka/producer.sh
@@ -9,24 +9,35 @@ IMAGE='wurstmeister/kafka:latest'
 BROKERS="${KAFKA_BROKERS:-localhost:9092}"
 MESSAGE_FILE=/dev/stdin
 TOPIC="${KAFKA_TOPIC:-$(whoami)-test}"
+ACK_ALL=0
 
 usage() {
-  echo 'Usage: consumer [OPTIONS]'
+  echo 'Usage: producer [OPTIONS]'
   echo ''
   echo 'OPTIONS:'
   echo ''
+  echo '-a, --ack-all        Require acknowledgements from multiple nodes'
+  echo '                     (equal to min.insync.replicas in broker settings)'
+  echo ''
   echo '-b, --brokers,'
-  echo '--bootstrap-server   The kafka broker list to initially use to identify the cluster.'
+  echo '--bootstrap-server   The kafka broker list to initially use to identify'
+  echo '                     the cluster.'
   echo '                     You can also use the $KAFKA_BROKERS var.'
   echo ''
-  echo '-f                   The file to read; should contain one message per line. Defaults to stdin'
+  echo '-f                   The file to read; should contain one message per '
+  echo '                     line. Defaults to stdin'
   echo ''
-  echo '-t, --topic TOPIC    The kafka topic to spaff; you can also use the $KAFKA_TOPIC env var'
+  echo '-t, --topic TOPIC    The kafka topic to write to; you can also use the '
+  echo '                     $KAFKA_TOPIC env var.'
   echo "                     If neither is set, defaults to $(whoami)-test"
 }
 
 while [[ "$1" != '' ]]; do
   case "$1" in
+    -a|--ack-all)
+      shift
+      ACK_ALL=1
+      ;;
     -b|--brokers|--bootstrap-server)
       shift
       BROKERS="$1"
@@ -64,8 +75,14 @@ else
   echo "${YELLOW}Submitting messages from $MESSAGE_FILE to $TOPIC at $BROKERS...$RESET"
 fi
 
+EXTRA_PROPS=''
+if [[ "$ACK_ALL" == 1 ]]; then
+  EXTRA_PROPS="$EXTRA_PROPS --producer-property request.required.acks=all"
+fi
+
 cat "$MESSAGE_FILE" | docker run -i --rm --entrypoint kafka-console-producer.sh --net host $IMAGE \
   --topic "$TOPIC" \
-  --broker-list "$BROKERS"
+  --broker-list "$BROKERS" \
+  $EXTRA_PROPS
 
 echo ''
