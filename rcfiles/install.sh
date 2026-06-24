5a0aa51266c11f3f595340b4d6f1306e1eba4cda
Rob Moore <giftiger.wunsch@gmail.com>
2026-06-24 14:23:49 +0100

    rcfiles: gitconfig_common

--- rcfiles/install.sh
+++ rcfiles/install.sh
@@ -4,19 +4,23 @@
 # You'll have to fix any failures yourself, as it won't attempt to overwrite anything
 # you already have in place to be on the safe side
 
-DIR=$(readlink -f "$(dirname "$0")")
+cd "$(dirname "$0")"
 
 install_rcfile() {
   f=$(basename "$1")
   echo -n "Installing rcfiles/$f... "
-  ln -s "$1" "$HOME/.$f"
+  ln -sT $(readlink -f "$1") "$HOME/.$f"
 
   if [[ "$?" == 0 ]]; then
     echo ''
   fi
 }
 
-find "$DIR" -maxdepth 1 | grep -Fv 'install.sh' | grep -Fv 'kitty' | grep -Ev '\.swp$' | while read f; do
+gc() {
+  git config --global "$1" "$2"
+}
+
+ls -1 | grep -Fv 'install.sh' | grep -Fv 'kitty' | grep -Ev '\.swp$' | while read f; do
   install_rcfile "$f"
 done
 
@@ -25,4 +29,7 @@ mkdir -p "$HOME/.config/kitty" && ln -s "$DIR/kitty/kitty.conf" "$HOME/.config/k
 mkdir -p "$HOME/.kitty" && touch "$HOME/.kitty/default.session"
 
 echo 'Setting default gitignore_global...'
-git config --global core.excludesfile $HOME/.gitignore_global
+gc core.excludesfile "$HOME/.gitignore_global"
+
+echo 'Adding gitconfig include...'
+gc include.path "$HOME/.gitconfig_common"
